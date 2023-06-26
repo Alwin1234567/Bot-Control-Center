2fbdd7a8e2ab35e59a5138c414db1e1b793cd441
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-06-26 18:25:25 +0200

    nice server stop

--- Console.py
+++ Console.py
@@ -6,6 +6,7 @@ import logging
 from logger import setup_logger
 from asyncio import sleep as asleep
 import sys
+import requests
 """
 Body
 """
@@ -30,4 +31,17 @@ class Console:
     
     async def command_stop(self, context):
         await self.remove_msg(context.message)
+        await self.stop_server()
+        asleep(5)
         sys.exit()
+    
+    async def stop_server(self):
+        TOKEN = self.tokens["DBH_TOKEN"]
+        url = self.tokens["DBH_URL"]
+        headers = {
+            'Authorization': 'Bearer {}'.format(TOKEN),
+            'Content-Type': 'application/json',
+            'Accept': 'Application/vnd.pterodactyl.v1+json'
+        }
+        data = '{ "signal": "kill" }'
+        requests.post(url, headers=headers, data=data)
