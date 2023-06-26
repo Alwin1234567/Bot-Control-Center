4ca157b5810bf918b4204bb4b1203a6e8705f77f
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-06-26 16:34:19 +0200

    setup part 1

---
+++ SubBot.py
@@ -0,0 +1,12 @@
+from abc import ABC, abstractmethod
+
+class SubBot(ABC):
+    
+    def __init__(self):
+        pass
+    
+    def start_bot(self):
+        pass
+    
+    def stop_bot(self):
+        pass
