7dbf469eebe2e32ce6748035d1f4d67e7368ed5d
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-06-26 16:50:03 +0200

    Setup part 3

--- Console.py
+++ Console.py
@@ -17,7 +17,7 @@ async def on_ready():
     logger.warning("bot is now online")
     logger.info("bot started with name: {} and id: {}".format(client.user.name, client.user.id))
 
-
+setup_logger("main")
 logger = logging.getLogger("main")
 TOKEN = json.load(open("tokens.json"))["Eden"]
 client.run(TOKEN)
