977e3611bce081cca10af4aaeacd9b10509b80c2
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-06-26 19:56:31 +0200

    create console msg

--- Console.py
+++ Console.py
@@ -7,13 +7,20 @@ from logger import setup_logger
 from asyncio import sleep as asleep
 import sys
 import requests
+from discord.ui import Button, View
+import discord
 """
 Body
 """
 
 
 class Console:
+    instance = None
     
+    def __new__(cls, client):
+        if cls.instance is None:
+            cls.instance = super().__new__(cls)
+        return cls.instance
     
     def __init__(self, client):
         setup_logger("main")
@@ -22,7 +29,16 @@ class Console:
         self.config = json.load(open("config.json"))
         self.tokens = json.load(open("tokens.json"))
         self.client = client
+        self.bots = self.config["Bots"]
         
+    
+    async def event_on_ready(self):
+        self.logger.info("bot is now online")
+        self.logger.info("bot started with name: {} and id: {}".format(self.client.user.name, self.client.user.id))
+        print(self.client.guilds)
+        self.console_channel = discord.utils.get(self.client.guilds.channels, name = "console")
+        await self.create_console_msg()
+    
     
     async def remove_msg(self, msg, delay = 0):
         if delay > 0: asleep(delay)
@@ -45,3 +61,11 @@ class Console:
         }
         data = '{ "signal": "kill" }'
         requests.post(url, headers=headers, data=data)
+        
+    
+    async def create_console_msg(self):
+        view = View()
+        for bot in self.bots.values():
+            view.add_item(Button(style = discord.ButtonStyle.secondary, label = bot["name"], emoji = bot["emoji"]))
+        await self.console_channel.send("button?", view = view)
+
--- Main.py
+++ Main.py
@@ -13,14 +13,11 @@ body
 client = dec.Bot(command_prefix = json.load(open("config.json"))["BOT_PREFIX"], intents=discord.Intents.all())
 
 @client.event
-async def on_ready():
-    console.logger.info("bot is now online")
-    console.logger.info("bot started with name: {} and id: {}".format(console.client.user.name, console.client.user.id))
+async def on_ready(): await console.event_on_ready()
 
 
 @client.command(brief = "stops all bots", description = "will try to stop all bots, if they don't comply, will force quit in 1 minute")
-async def stop(context):
-    await console.command_stop(context)
+async def stop(context): await console.command_stop(context)
     
 
 
