f784aae19dc25c5a5d2092c7ad42d3153a9476cf
Alwin1234567 <alwinvanderpaardt@gmail.com>
2023-06-28 17:08:52 +0200

    and another

--- Send_lore.py
+++ Send_lore.py
@@ -15,14 +15,14 @@ class Send_lore(SubBot):
         super().__init__(client, token)
 
     async def start_bot(self):
-        self.cog = await self.client.add_cog(Send_lore_events(self.client))
+        await self.client.add_cog(Send_lore_events(self.client))
         await super().start_bot()
-        # self.cog = await self.client.get_cog("Send_lore_events")
+        self.cog = await self.client.get_cog("Send_lore_events")
     
     
     async def trigger(self): 
         print(1)
-        print(type(self.cog))
+        print(self.client.cogs)
         await self.cog.send_message()
         
 class Send_lore_events(commands.Cog):
